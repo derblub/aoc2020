fd68566c9298e016d9505367f10fd45743b09670
Daniel <daniel@pushingpixels.at>
2020-12-01 09:44:10 +0100

    finished day 01 (part two)

--- 01_report-repair/main.py
+++ 01_report-repair/main.py
@@ -10,9 +10,24 @@ with open(os.path.join(sys.path[0], "input.txt"), "r") as file:
     lines = [int(line.rstrip("\n")) for line in file]
 
 final_sum = 2020
-for a in range(len(lines)):
-    for b in range(len(lines)):
-        if lines[a] + lines[b] == final_sum:
-            output = lines[a] * lines[b]
-            print(output)
-            exit(1)
+
+def get_output_1(data):
+    for a in range(len(data)):
+        for b in range(len(data)):
+            if data[a] + data[b] == final_sum:
+                output = data[a] * data[b]
+                print(output)
+                return
+
+def get_output_2(data):
+    for a in range(len(data)):
+        for b in range(len(data)):
+            for c in range(len(data)):
+                if data[a] + data[b] + data[c] == final_sum:
+                    output = data[a] * data[b] * data[c]
+                    print(output)
+                    return
+
+
+get_output_1(lines)
+get_output_2(lines)
